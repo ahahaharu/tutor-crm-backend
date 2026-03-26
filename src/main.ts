0e4980d60367925e173f6d39254f337c3891d3eb
Andrey <112519966+ahahaharu@users.noreply.github.com>
2026-03-26 23:16:12 +0300

    chore(core): configure CORS for frontend integration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-// Импортируем классы для Swagger
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3001'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Tutor CRM API')
