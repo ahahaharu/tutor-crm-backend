aa00b537aa792992441711d2d7e241ba25fa846b
Andrey <112519966+ahahaharu@users.noreply.github.com>
2026-03-26 23:06:39 +0300

    docs(swagger): setup automatic API documentation and Swagger UI

--- src/students/dto/create-student.dto.ts
+++ src/students/dto/create-student.dto.ts
@@ -1,10 +1,19 @@
 import { IsString, IsOptional, Length } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'; // Импортируем из Swagger
 
 export class CreateStudentDto {
+  @ApiProperty({
+    example: 'Anton Secure',
+    description: 'The full name of the student',
+  })
   @IsString()
   @Length(2, 50, { message: 'name must be between 2 and 50 characters' })
   name: string;
 
+  @ApiPropertyOptional({
+    example: '@anton_telegram',
+    description: 'Contact information (Telegram, phone, etc.)',
+  })
   @IsOptional()
   @IsString()
   contactInfo?: string;
