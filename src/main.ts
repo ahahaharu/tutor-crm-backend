f4fb0142457e51459a3465e823134c7a2cfcb16c
Andrey <andreybogdanovich06@gmail.com>
2026-04-30 14:02:14 +0300

    feat: integrate pino logger for structured JSON logging

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.useLogger(app.get(Logger));
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
